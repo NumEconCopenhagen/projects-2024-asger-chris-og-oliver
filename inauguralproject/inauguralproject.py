7cafb166846652235410a9adb3b655b65c353bc0
mjv474 <mjv474@alumni.ku.dk>
2024-03-24 18:48:22 +0100

    sammensætning af spm

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,6 +1,7 @@
 from types import SimpleNamespace
 import numpy as np
 import matplotlib.pyplot as plt  
+from scipy import optimize
 
 class ExchangeEconomyClass:
 
@@ -36,6 +37,7 @@ class ExchangeEconomyClass:
 
         return util_B
 
+
     def demand_A(self,p1):
         par = self.par
         I_A = p1*par.w1A + par.w2A
@@ -43,6 +45,7 @@ class ExchangeEconomyClass:
         x2A_star = (1-par.alpha)*I_A
 
         return x1A_star,x2A_star
+
 
     def demand_B(self,p1):
         par = self.par
@@ -64,7 +67,14 @@ class ExchangeEconomyClass:
 
         return eps1,eps2
     
-    def paretoC(self, x1A_vec, x2A_vec, uA_bar, uB_bar):
+
+    def paretoC(self, N = 75):
+        N = N
+        x1A_vec = np.linspace(0,1,N)
+        x2A_vec = np.linspace(0,1,N)
+        uA_bar = self.utility_A(self.par.w1A, self.par.w2A)
+        uB_bar = self.utility_B(self.par.w1B, self.par.w2B)
+
         kombinationer = []
         for x1a in x1A_vec:
             for x2a in x2A_vec:
@@ -75,6 +85,61 @@ class ExchangeEconomyClass:
                 if uA >= uA_bar and uB >= uB_bar:
                     kombinationer.append((x1a, x2a))
         return kombinationer
+    
+    def P_1(self, N = 75):
+        N = N
+        P_1 = [0.5]
+        i = 1
+        while P_1[-1] < 2.5:
+            P_1.append(0.5 + (2*i)/N)
+            i += 1 
+        return P_1
+
+
+    def plot_edgeworth(self, N = 75):
+        par = self.par
+
+        # Create pareto combinations
+        kombinationer = self.paretoC(N)
+        x1, x2 = zip(*kombinationer)
+
+        # 4 Plot settings
+        plt.rcParams.update({"axes.grid":True,"grid.color":"black","grid.alpha":"0.25","grid.linestyle":"--"})
+        plt.rcParams.update({'font.size': 14})
+
+        # a. total endowment
+        w1bar = 1.0
+        w2bar = 1.0
+
+        # b. figure set up
+        fig = plt.figure(frameon=True,figsize=(6,6), dpi=100)
+        ax_A = fig.add_subplot(1, 1, 1)
+        
+        ax_A.set_xlabel("$x_1^A$")
+        ax_A.set_ylabel("$x_2^A$")
+
+        temp = ax_A.twinx()
+        temp.set_ylabel("$x_2^B$")
+        ax_B = temp.twiny()
+        ax_B.set_xlabel("$x_1^B$")
+        ax_B.invert_xaxis()
+        ax_B.invert_yaxis()
+
+        ax_A.scatter(x1,x2,marker='s',color='royalblue',label='pareto improvements', s = 50)
+        ax_A.scatter(self.par.w1A, self.par.w2A ,marker='s',color='black',label='endowment', s = 50)
+
+        # limits
+        ax_A.plot([0,w1bar],[0,0],lw=2,color='black')
+        ax_A.plot([0,w1bar],[w2bar,w2bar],lw=2,color='black')
+        ax_A.plot([0,0],[0,w2bar],lw=2,color='black')
+        ax_A.plot([w1bar,w1bar],[0,w2bar],lw=2,color='black')
+
+        ax_A.set_xlim([-0.1, w1bar + 0.1])
+        ax_A.set_ylim([-0.1, w2bar + 0.1])    
+        ax_B.set_xlim([w1bar + 0.1, -0.1])
+        ax_B.set_ylim([w2bar + 0.1, -0.1])
+
+        ax_A.legend(frameon=True,bbox_to_anchor=(1.1,1.0));
     
     def market_clear(self, P_1):
         e1 = 10
@@ -89,7 +154,7 @@ class ExchangeEconomyClass:
                 p1_best = p1
         return e1_best, e2_best, p1_best
 
-    def plot_error(self, p1, N):
+    def plot_error(self, p1, N = 75):
         # i. market erros from P_1
         N = N
         P_1 = np.linspace(1e-4, 3, N)
@@ -114,44 +179,168 @@ class ExchangeEconomyClass:
         ax.set_xlim([0.25,2.5])
         ax.legend()
 
+    def A_sets_price(self, P_1):
+        uA_0 = -np.inf
+        for p1 in P_1:
+            x1_A = 1 - self.demand_B(p1)[0]
+            x2_A = 1 - self.demand_B(p1)[1]
+            uA_now = self.utility_A(x1_A, x2_A)
+            if uA_now > uA_0:
+                uA_0 = uA_now
+                uA_best = uA_now
+                x1_A_best = x1_A
+                x2_A_best = x2_A
+                p1_A_best = p1
+
+        return x1_A_best, x2_A_best, uA_best, p1_A_best
     
+    def A_sets_price_optimize(self):
+        # a. define objective function to minimize as a function of p
+        obj = lambda p: -self.utility_A(1 - self.demand_B(p)[0], 1 - self.demand_B(p)[1]) 
 
+        # b. intitial guess and call optimizer
+        p0 = 1
+        result = optimize.minimize(obj, p0, method='SLSQP')
 
-def plot_edgeworth(x1, x2, w1a, w2a):
-    plt.rcParams.update({"axes.grid":True,"grid.color":"black","grid.alpha":"0.25","grid.linestyle":"--"})
-    plt.rcParams.update({'font.size': 14})
+        util = -result.fun
+        p = result.x[0]
+        x1a = 1 - self.demand_B(p)[0]
+        x2a = 1 - self.demand_B(p)[1]
+ 
 
-    # a. total endowment
-    w1bar = 1.0
-    w2bar = 1.0
-
-    # b. figure set up
-    fig = plt.figure(frameon=True,figsize=(6,6), dpi=100)
-    ax_A = fig.add_subplot(1, 1, 1)
+        return x1a, x2a, util, p
     
-    ax_A.set_xlabel("$x_1^A$")
-    ax_A.set_ylabel("$x_2^A$")
+    def plot_utility_A(self, P_1):
+        # Create an empty list to store the utility values
+        utility_values = []
+        A_sets_price = self.A_sets_price(P_1)
 
-    temp = ax_A.twinx()
-    temp.set_ylabel("$x_2^B$")
-    ax_B = temp.twiny()
-    ax_B.set_xlabel("$x_1^B$")
-    ax_B.invert_xaxis()
-    ax_B.invert_yaxis()
+        # Iterate over each value of p1 in P_1
+        for p1 in P_1:
+            x1_A = 1 - self.demand_B(p1)[0]
+            x2_A = 1 - self.demand_B(p1)[1]
+            # Calculate the utility for the given p1
+            utility = self.utility_A(x1_A, x2_A)
+            utility_values.append(utility)
 
-    ax_A.scatter(x1,x2,marker='o',color='royalblue',label='pareto improvements')
-    ax_A.scatter(w1a, w2a ,marker='s',color='black',label='endowment', s = 50)
+        # Plot the utility values
+        plt.plot(P_1, utility_values, label = "utility value")
+        plt.scatter(A_sets_price[3], A_sets_price[2], c="red", zorder=10, label = "maximum")  
+        plt.ylabel('Utility')
+        plt.xlabel('p1')
+        plt.title('Utility of A for different prices')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
-    # limits
-    ax_A.plot([0,w1bar],[0,0],lw=2,color='black')
-    ax_A.plot([0,w1bar],[w2bar,w2bar],lw=2,color='black')
-    ax_A.plot([0,0],[0,w2bar],lw=2,color='black')
-    ax_A.plot([w1bar,w1bar],[0,w2bar],lw=2,color='black')
+    def A_market_maker(self, N = 75):
+        kombinationer = self.paretoC(N)
+        util_0 = 0
 
-    ax_A.set_xlim([-0.1, w1bar + 0.1])
-    ax_A.set_ylim([-0.1, w2bar + 0.1])    
-    ax_B.set_xlim([w1bar + 0.1, -0.1])
-    ax_B.set_ylim([w2bar + 0.1, -0.1])
+        for i in kombinationer:
+            util_now = self.utility_A(*i)
+            if util_now > util_0:
+                util_0 = util_now
+                util_best = util_now
+                x1_best, x2_best = i
 
-    ax_A.legend(frameon=True,bbox_to_anchor=(1.1,1.0));
+        return x1_best, x2_best, util_best
+    
+    def A_market_maker_optimize(self):
+        
+        # a. define objective function to minimize as a function of x1 and x2
+        def value_of_choice(x):
+            return -self.utility_A(x[0], x[1]) 
+        
+        uB_bar = self.utility_B(self.par.w1B, self.par.w2B)
 
+        # b. define bounds and restrictions
+        bounds = [(0, 1), (0, 1)]
+        constraints = ({'type': 'eq', 'fun': lambda x: self.utility_B(1 - x[0], 1 - x[1]) - uB_bar})
+
+        # c. intitial guess and call optimizer
+        guess = [self.par.w1A, self.par.w2A]
+        result = optimize.minimize(value_of_choice, guess, bounds = bounds, constraints = constraints, method='SLSQP')
+
+        x1_best = result.x[0]
+        x2_best = result.x[1]
+        util_best = -result.fun
+        return x1_best, x2_best, util_best
+    
+    def plot_edgeworth2(self, N = 75, u_a = 0.5713, u_b = 0.4865, p1 = 0.9444):
+        plt.rcParams.update({"axes.grid":True,"grid.color":"black","grid.alpha":"0.25","grid.linestyle":"--"})
+        plt.rcParams.update({'font.size': 14})
+
+        par = self.par
+
+        # a. total endowment
+        w1bar = 1.0
+        w2bar = 1.0
+
+        # b. figure set up
+        fig = plt.figure(frameon=False,figsize=(6,6), dpi=100)
+        ax_A = fig.add_subplot(1, 1, 1)
+
+        ax_A.set_xlabel("$x_1^A$")
+        ax_A.set_ylabel("$x_2^A$")
+
+        temp = ax_A.twinx()
+        temp.set_ylabel("$x_2^B$")
+        ax_B = temp.twiny()
+        ax_B.set_xlabel("$x_1^B$")
+        ax_B.invert_xaxis()
+        ax_B.invert_yaxis()
+
+        kombinationer = self.paretoC()
+        x1, x2 = zip(*kombinationer)
+
+        ax_A.scatter(x1,x2,marker='o',color='royalblue',label='pareto improvements')
+        ax_A.scatter(par.w1A, par.w2A,marker='s',color='black',label='endowment', s = 50)
+        
+        P_1 = self.P_1(N)
+
+        # Allocations from 3-5
+        # Market clearing allocation
+        allocation_3 = self.market_clear(P_1)
+        market_clearing = self.demand_A(allocation_3[2])
+
+        # Price setter
+        allocation_4 = self.A_sets_price(P_1)
+
+        # Market maker
+        allocation_5 = self.A_market_maker()
+
+        # Allocations 3)-5)
+        ax_A.scatter(market_clearing[0], market_clearing[1], color = "red", label = "market clearing allocation")
+        ax_A.scatter(allocation_4[0], allocation_4[1], color = "yellow", label = "A is pricesetter")
+        ax_A.scatter(allocation_5[0], allocation_5[1], color = "m", label = "A is market maker")
+
+        # indifference curves
+        u_a = u_a
+        x1 = np.linspace(1e-8, 2, N)
+        x2 = (u_a*x1**(-self.par.alpha))**(1/(1-self.par.alpha))
+
+        u_b = u_b
+        x1b = np.linspace(1e-8, 2, N)
+        x2b = (u_b*x1b**(-self.par.beta))**(1/(1-self.par.beta))
+
+        # Budget line
+        p1 = p1
+        x2_budget = p1*self.par.w1A + self.par.w2A - p1*x1
+
+        ax_A.plot(x1, x2, color = "black", label = "indifference curve A")
+        ax_A.plot(1-x1b, 1-x2b, color = "blue", label = "indifference curve B")
+        ax_A.plot(x1, x2_budget, color = "orange", label = "budget line")
+
+        # limits
+        ax_A.plot([0,w1bar],[0,0],lw=2,color='black')
+        ax_A.plot([0,w1bar],[w2bar,w2bar],lw=2,color='black')
+        ax_A.plot([0,0],[0,w2bar],lw=2,color='black')
+        ax_A.plot([w1bar,w1bar],[0,w2bar],lw=2,color='black')
+
+        ax_A.set_xlim([-0.1, w1bar + 0.1])
+        ax_A.set_ylim([-0.1, w2bar + 0.1])    
+        ax_B.set_xlim([w1bar + 0.1, -0.1])
+        ax_B.set_ylim([w2bar + 0.1, -0.1])
+
+        ax_A.legend(frameon=True,bbox_to_anchor=(1.1,1.0));
