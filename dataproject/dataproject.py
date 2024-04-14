22576d9a38ad02bcd2875de9a3be00b2c97b81b1
Oliver Ding <oliver_ding@hotmail.com>
2024-04-14 15:26:31 +0200

    Moved beta_table from dataproject.ipynb to dataproject.py

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -2,3 +2,41 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+class Dataproject:
+
+    
+    def beta_table(results):
+    '''Makes table with estimation coefficients and t-values'''
+    
+    # List to save beta values
+    beta_values = {}
+
+    for key, result in results.items():
+        beta_values[key] = round(result.params['Mkt-RF'], 3)
+
+    # Extract constant values
+    constant_values = {}
+
+    for key, result in results.items():
+        constant_values[key] = round(result.params['const'], 3)
+
+    # Calculate t-values for the 'Mkt-RF' coefficient
+    t_values = {}
+    for key, result in results.items():
+        t_values[key] = round(result.params['Mkt-RF'] / result.bse['Mkt-RF'], 2)
+    # Calculate t-values for the constant coefficient
+
+    t_values_a = {}
+    for key, result in results.items():
+        t_values_a[key] = round(result.params['const'] / result.bse['const'], 2)    
+
+    beta_table = pd.DataFrame(list(beta_values.items()), columns=['Stock', 'Beta'])
+
+    # Add constant values and t-values to the DataFrame
+    beta_table['t_Beta'] = t_values.values()
+    beta_table['cons'] = constant_values.values()
+    beta_table['t_cons'] = t_values_a.values()
+
+    return beta_table
+
